1550acdd25bfaf1ea52eca24a8ee57c05708306b
Stephen G Tuggy <sgt@stephengtuggy.com>
2016-03-27 10:58:33 -0700

    Add license (GPLv3)

--- ContactsSquared/Startup.cs
+++ ContactsSquared/Startup.cs
@@ -1,4 +1,23 @@
-﻿using Microsoft.Owin;
+﻿/**
+ * Copyright 2015-2016 Stephen G. Tuggy, Jose Cabrera Jr., and Joshua Weston.
+ * 
+ * This file is part of ContactsSquared.
+ *
+ * ContactsSquared is free software: you can redistribute it and/or modify it 
+ * under the terms of the GNU General Public License as published by the Free 
+ * Software Foundation, either version 3 of the License, or (at your option) 
+ * any later version.
+ *
+ * ContactsSquared is distributed in the hope that it will be useful, but 
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or 
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for 
+ * more details.
+ *
+ * You should have received a copy of the GNU General Public License along with 
+ * ContactsSquared.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+using Microsoft.Owin;
 using Owin;
 
 [assembly: OwinStartupAttribute(typeof(ContactsSquared.Startup))]
